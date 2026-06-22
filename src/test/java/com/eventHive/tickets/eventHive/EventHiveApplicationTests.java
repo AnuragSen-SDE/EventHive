cc950de967d1faf657eabe0c33130ed3ab8e6bfe
Anurag Sen <anuragsen@Anurags-MacBook-Pro.local>
2026-06-22 18:00:53 +0530

    first commit

---
+++ src/test/java/com/eventHive/tickets/eventHive/EventHiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eventHive.tickets.eventHive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventHiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
