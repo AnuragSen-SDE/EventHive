c26601dd37b22333a2471b714df1f2ff6def4ce9
Anurag Sen <anurag007sen@gmail.com>
2026-07-01 18:37:57 +0530

    chor: working to implement the cicd pipeline

--- src/main/java/com/eventHive/tickets/eventHive/EventHiveApplication.java
+++ src/main/java/com/eventHive/tickets/eventHive/EventHiveApplication.java
@@ -3,10 +3,13 @@ package com.eventHive.tickets.eventHive;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class EventHiveApplication {
 
 	public static void main(String[] args) {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Kolkata"));
 		SpringApplication.run(EventHiveApplication.class, args);
 	}
 
